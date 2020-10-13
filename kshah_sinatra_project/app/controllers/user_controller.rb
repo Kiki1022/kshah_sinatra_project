bd6c8fc71d5d5081d9b93a84d2f1e36bd3d286e6
kinnari <kinnari1022@yahoo.com>
2020-10-12 22:43:17 -0400

    changed password to password digest in users table

--- kshah_sinatra_project/app/controllers/user_controller.rb
+++ kshah_sinatra_project/app/controllers/user_controller.rb
@@ -1,12 +1,28 @@
 class UserController < ApplicationController
     
     get '/users/signup' do 
-        erb :'/users/signup'
+        erb :'/users/signup' # do i have my user object here? no. we don't need session here b/c we don't knonw about user yet
     end
     
-    post '/users/signup' do
-        @user = User.create(params)
+    post '/users/signup' do #now we created user object, they have id we can utilize
+        @user = User.create(
+            username: params[:username],
+            password: params[:password]
+        )
+        session[:user_id] = @user.id
         redirect "/users/#{@user.id}" 
+    end
+
+    get '/users/login' do
+        #if user is logged in 
+        #redirect to their home page
+        #else, show them the login form
+        erb :"/users/login"
+    end
+
+    post '/users/login' do
+      @user =  User.find_by(username: params[:username])   #find will result to finding id number vs find_by will find a certain field
+        
     end
 
     get '/users/:id' do
