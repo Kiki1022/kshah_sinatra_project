9659a19fee54c11723538e31c103684a1971d60e
kinnari <kinnari1022@yahoo.com>
2020-10-15 16:15:14 -0400

    created new users table bc of Bcrypt error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201013024017) do
+ActiveRecord::Schema.define(version: 20201015200743) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "name"
